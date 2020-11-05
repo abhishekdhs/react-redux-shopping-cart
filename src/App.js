eeef253d8ffe82d6c005af2519032dad26c58919
abhishekdhs <abhishek.patel913@gmail.com>
2020-11-05 09:39:02 +0530

    added redux products

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 //feature 1
 import React from "react"
+import { Provider } from "react-redux";
 import Cart from "./components/Cart";
 import Filter from "./components/Filter";
 import Products from "./components/Products";
 import data from "./data.json"
+import store from "./store"
 
 class App extends React.Component {
 
@@ -86,7 +88,10 @@ class App extends React.Component {
 
 
   render(){
+    
     return (
+
+      <Provider store={store}>
       <div className="grid-container">
         <header>
           <a href='/'>React Shopping Cart</a>
@@ -116,6 +121,7 @@ class App extends React.Component {
         </main>
         <footer>All right reserved</footer>
       </div>
+      </Provider>
     );
   }
   }
