4167eb034fe9a8c72b96249b81bf3e08fed8ad81
abhishekdhs <abhishek.patel913@gmail.com>
2020-11-05 11:25:19 +0530

    added redux to cart

--- src/App.js
+++ src/App.js
@@ -8,47 +8,7 @@ import store from "./store"
 
 class App extends React.Component {
 
-  constructor(){
-    super();
-    this.state = {
-      
-      cartItems: localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : [],
-
-    }
-  }
-
-    createOrder = (order) => {
-      alert("Need to save order for " + order.name);
-    }
-
-  removeFromCart=(product)=>{
-    console.log("remove from cart clicked")
-    const cartItems = this.state.cartItems.slice();
-    this.setState({
-      cartItems: cartItems.filter((x)=>x._id !== product._id)
-    });
-    localStorage.setItem("cartItems", JSON.stringify(cartItems.filter((x) => x._id !== product._id)));
-    
-  }
-
-  addToCart = (product) => {
-    const cartItems = this.state.cartItems.slice();
-    let alreadyIncart = false;
-    cartItems.forEach(item => {
-      if(item._id === product._id){
-        item.count++;
-        alreadyIncart = true;
-      }
-    });
-    if(!alreadyIncart){
-      cartItems.push({...product, count:1})
-    }
-    this.setState({cartItems});
-    localStorage.setItem("cartItems", JSON.stringify(cartItems));
-  }
-
-
-
+  
 
   render(){
     
@@ -65,15 +25,12 @@ class App extends React.Component {
             <div className="main">
               <Filter />
               <Products 
-              addToCart = {this.addToCart}
+             
               />
             </div>
 
             <div className="sidebar">
-              <Cart cartItems={this.state.cartItems} 
-              removeFromCart ={this.removeFromCart}
-              createOrder = {this.createOrder}
-              />
+              <Cart />
             </div>
           </div>
         </main>
--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import thunk from "redux-thunk";
+import { cartReducer } from "./reducers/cartReducers";
 import { productsReducer } from "./reducers/productReducers";
 
 
@@ -8,6 +9,7 @@ const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   combineReducers({
     products: productsReducer,
+    cart:cartReducer,
    
   }),
   initialState,
